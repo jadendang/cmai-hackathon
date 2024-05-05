d5777624282a5047254269bf3cac779b101e9dd6
jadendang <127361925+jadendang@users.noreply.github.com>
2024-05-05 16:00:55 -0700

    Add files via upload

--- octo_ai.py
+++ octo_ai.py
@@ -6,35 +6,26 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import Milvus
+from langchain_community.document_loaders.csv_loader import CSVLoader
 import os
 
 load_dotenv()
 
 OCTOAI_API_TOKEN = os.environ["OCTOAI_API_TOKEN"]
 
-url = "https://en.wikipedia.org/wiki/Star_Wars"
-
-headers_to_split_on = [
-    ("h1", "Header 1"),
-    ("h2", "Header 2"),
-    ("h3", "Header 3"),
-    ("h4", "Header 4"),
-    ("div", "Divider")
-]
-
-html_splitter = HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-
-html_header_splits = html_splitter.split_text_from_url(url)
-
-chunk_size = 1024
-chunk_overlap = 128
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size,
-    chunk_overlap=chunk_overlap,
+loader = CSVLoader(
+    file_path="/Users/jaden/Desktop/hackathon may 2024/cmai-hackathon/testhousing.csv",
+    csv_args={
+        "delimiter": ",",
+        "quotechar": '"',
+        "fieldnames": ["bedroom","baths","fullBaths","stories","city","state","zipCode","countryCode","propertyId","listingId","timeZone","hasVirtualTour","has3dTour","description","price","hoaValue","sqFt","pricePerSqFt","location","yearBuilt"],
+    },
 )
 
-# Split
-splits = text_splitter.split_documents(html_header_splits)
+data = loader.load()
+
+print(data[0])
+
 
 llm = OctoAIEndpoint(
         model_name="llama-2-13b-chat-fp16",
@@ -47,18 +38,22 @@ llm = OctoAIEndpoint(
 embeddings = OctoAIEmbeddings(endpoint_url="https://text.octoai.run/v1/embeddings")
 
 vector_store = Milvus.from_documents(
-    splits,
+    data,
     embedding=embeddings,
     connection_args={"host": "localhost", "port": 19530},
-    collection_name="starwars"
+    collection_name="house_listings"
 )
 
 retriever = vector_store.as_retriever()
 
-template="""You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+print("context retrieved:")
+print(retriever)
+
+template="""You are a document-scanning assistant for question-answering tasks. Listings are given as a csv file with headers as the first row and data values in the rest of the rows. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use five sentences maximum and keep the answer concise but descriptive.
 Question: {question} 
 Context: {context} 
 Answer:"""
+
 prompt = ChatPromptTemplate.from_template(template)
 
 chain = (
@@ -68,4 +63,5 @@ chain = (
     | StrOutputParser()
 )
 
-print(chain.invoke("Who is Luke's Father?"))
+
+print(chain.invoke("Can you provide listings that include a hot tub, was built before 1992, 3 beds, 2 baths, single story, roughly 1800 sq ft. I also want the listings to have a recently remodeled kitchen."))
